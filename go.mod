f1548fae9d11edb783f102fa64447c07a23314b6
unknown <wongsathon@2fellows.com>
2022-05-04 12:10:33 +0700

    config mail with yaml file.

--- go.mod
+++ go.mod
@@ -4,15 +4,13 @@ go 1.16
 
 require (
 	github.com/fluk27/testgo2020 v0.0.0-20210128044226-f7282dee452b
+	github.com/go-playground/validator/v10 v10.11.0
 	// github.com/fluk27/testgo2020 v0.0.0-20210128044226-f7282dee452b
 	github.com/gofiber/fiber/v2 v2.12.0
 	github.com/google/go-cmp v0.5.6 // indirect
 	github.com/klauspost/compress v1.13.0 // indirect
-	github.com/kr/pretty v0.1.0 // indirect
 	github.com/olivere/elastic/v7 v7.0.24
 	github.com/pkg/errors v0.9.1
-	golang.org/x/sys v0.0.0-20210603125802-9665404d3644 // indirect
-	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 	gopkg.in/validator.v2 v2.0.0-20210331031555-b37d688a7fb0
-	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b
+	gopkg.in/yaml.v2 v2.3.0
 )
--- helper/email/send_mail_services.go
+++ helper/email/send_mail_services.go
@@ -1,39 +1,58 @@
 package services
 
 import (
+	"errors"
 	"io/ioutil"
 	"log"
 
-	yaml "gopkg.in/yaml.v3"
+	validator "github.com/go-playground/validator/v10"
+	yaml "gopkg.in/yaml.v2"
 )
 
 type SendMailServices struct {
-	Sender     string
-	DataMail   string
-	HeaderMail string
+	Sender     string `validate:"required"`
+	DataMail   string `validate:"required"`
+	HeaderMail string `validate:"required"`
 }
 
 type mailConfig struct {
-	user     string `yaml:"SENDER"`
-	password string `yaml:"SECRET"`
-	port     string `yaml:"PORT"`
-	host     string `yaml:"SERVER"`
+	User     string `yaml:"SENDER" validate:"required"`
+	Password string `yaml:"SECRET" validate:"required"`
+	Port     string `yaml:"PORT" validate:"required"`
+	Host     string `yaml:"SERVER" validate:"required"`
 }
-// vaildateConfig 
-func (sm *SendMailServices) vaildateConfig() {
-	buf, err := ioutil.ReadFile("./configs/g_emaiL.env.yaml")
+
+// vaildateConfig
+func (sm *SendMailServices) vaildateConfig() (*mailConfig, error) {
+	buf, err := ioutil.ReadFile("./helper/email/configs/g_emaiL.env.yaml")
 	if err != nil {
-		log.Println("err read file yaml:",err.Error())
+		log.Println("err read file yaml:", err.Error())
+		return nil, errors.New(err.Error())
 	}
 
-	c := &mailConfig{}
-	err = yaml.Unmarshal(buf, c)
+	mailCon := &mailConfig{}
+	err = yaml.Unmarshal(buf, mailCon)
 	if err != nil {
-		log.Println("err map yaml to struct:",err.Error())
+		log.Println("err map yaml to struct:", err.Error())
+		return nil, errors.New(err.Error())
 	}
-
-	// return c, nil
+	var validate *validator.Validate
+	errr := validate.Struct(mailConfig)
+	if errr != nil {
+		return nil, err
+	}
+	return mailCon, nil
 }
-func (sm *SendMailServices) validateDataMail() {
+func (sm *SendMailServices) validateDataMail() error {
+	log.Println("dataMail:", sm)
+	return nil
+}
 
+func (sm *SendMailServices) SendMail() error {
+	_, err := sm.vaildateConfig()
+	if err != nil {
+		return errors.New(err.Error())
+	}
+	err = sm.validateDataMail()
+	return nil
 }
